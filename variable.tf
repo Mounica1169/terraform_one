caf7973490f343c8ec95637350658aeaf958734a
Mounica1169 <102153988+Mounica1169@users.noreply.github.com>
2026-02-22 14:03:28 +0530

    Add Terraform variables for AMI, instance type, volume, and count

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "it has ami_id"
+  type = string
+  default = "ami-0f3caa1cf4417e51b"
+}
+variable "itype" {
+  description = "it has instance type"
+  type = string
+  default = "t3.micro"
+}
+variable "volume" {
+  description = "it has volume"
+  type = Number
+  default = 10
+}
+variable "icount" {
+  type = Number
+  default = 1
+}
